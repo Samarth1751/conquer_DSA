d62700d543aa84245b92f08e0d40301f98a70e4a
Samarth1751 <samarthbisen17@gmail.com>
2025-06-19 04:11:16 +0530

    Remove Dupes from arr

---
+++ src/Arrays/Easy/RemoveDupes.java
@@ -0,0 +1,23 @@
+package Arrays.Easy;
+
+public class RemoveDupes {
+    public static void main(String[] args) {
+        int arr[] ={1,1,2,2,3,3};
+        System.out.println(RemoveDupes(arr));
+    }
+
+    // ise thoda ache se dekhna pdega
+    static int RemoveDupes(int arr[]){
+        int i =0;
+        for(int j =1; j< arr.length;j++){
+            if(arr[j] != arr[i]){
+                i++;
+                arr[i] = arr[j];
+            }
+
+        }
+        return i+1;
+    }
+
+
+}
